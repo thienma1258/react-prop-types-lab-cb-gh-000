52d1450e5d42993e62a9545a5bf74c069776a9c9
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-28 03:31:42 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,14 +4,14 @@ import PropTypes   from 'react-proptypes';
 class Product extends React.Component{
   function range(props, propName, componentName) {
     componentName = comopnentName || 'ANONYMOUS';
-  
+
     if (props[propName]) {
       let value = props[propName];
       if (typeof value === 'number') {
           return (value >= 80 && value <= 300) ? null : new Error(propName + ' in ' + componentName + " is not within 1 to 10");
       }
     }
-  
+
     // assume all ok
     return null;
   }
@@ -20,7 +20,7 @@ class Product extends React.Component{
     return(
 
 
-      
+
     )
   }
 }
@@ -34,6 +34,6 @@ Product.PropTypes={
 
 Product.defaultProps ={
   hasWatermark:false,
-  
+
 }
 export default Product
